8c947ee7d0453ab249c795ece3db53116a2c2867
marialoudiaz <loumariadiaz@gmail.com>
2024-08-11 00:34:36 +0200

    server

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const port = 443;
 
 app.use(cors({
 	origin: 'https://www.marialoudiaz.fr', // Permet uniquement cette origine
-	methods: ['GET', 'POST'],
+	methods: ['GET', 'POST', 'OPTIONS'],
 	allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
