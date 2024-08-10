e38207d4e50d5d90066973f6d57851c5fc9cdec1
marialoudiaz <loumariadiaz@gmail.com>
2024-08-11 00:23:31 +0200

    port

--- src/server.js
+++ src/server.js
@@ -6,13 +6,13 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = (443);
+const port = 443;
 
-app.use(cors({
-	origin: 'https://www.marialoudiaz.fr', // Permet uniquement cette origine
-	methods: ['GET', 'POST'],
-	allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+// app.use(cors({
+// 	origin: 'https://www.marialoudiaz.fr', // Permet uniquement cette origine
+// 	methods: ['GET', 'POST'],
+// 	allowedHeaders: ['Content-Type', 'Authorization'],
+// }));
 
 app.use(function(req, res, next) { 
 	res.header("Access-Control-Allow-Origin", "*"); 
